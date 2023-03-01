75f56bba91e9257a0dbe35efe677586bfc272633
1000dimensions <jaksonclyde@gmail.com>
2023-03-01 17:17:45 -0500

    login

--- conquers.js
+++ conquers.js
@@ -1,6 +1,10 @@
+const cbg = document.querySelector('#map')
+const ctx = cbg.getContext('2d')
+
 var time = Date.now();
 var days = time / 1000 / 60 / 60 / 24
 var min = time / 1000 / 60;
 var hours = time / 1000 / 60 / 60 / 24
 console.log(min);
-console.log(finMin);
+console.log(days);
+console.log(hours);
---
+++ login.js
@@ -0,0 +1,14 @@
+const bogga = document.getElementById('ogga');
+const pass = document.getElementById('pass').value;
+const uName = document.getElementById('Uname').value;
+var user = new Parse.Query("User");
+function retreiveU(){
+    if(uName != ""){   
+        const query = new.Parse.Query('User');
+        try {
+            const uname = await query.get(uName);
+        }catch(err){
+            throw err + "You got an error!"
+        }
+    }
+}

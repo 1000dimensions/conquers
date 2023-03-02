8d19917acc75e27498068dea108602869222522b
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-02 12:01:02 -0500

    Update this pray it works

--- login.js
+++ login.js
@@ -1,13 +1,18 @@
 const bogga = document.getElementById('ogga');
 const pass = document.getElementById('pass').value;
 const uName = document.getElementById('Uname').value;
-var user = new Parse.Query("User");
+var user = new Parse.Query("log");
 function retreiveU(){
     if(uName != ""){  
         try {
-            const uname = await user.get(uName);
+            const uname = await user.get(Uname);
+            const Pas = await user.get(Pass);
+            if(pass != Pas) {
+                wrong();
         }catch(err){
             throw err + "You got an error!"
         }
     }
 }
+function wrong() {
+    document.getElementById('Pass').innerHTML = "Wrong Password!";

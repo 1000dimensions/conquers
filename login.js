7849d39e8db1a57c7b205750fd540a68f034459a
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-03 12:44:09 -0500

    Try this. Looking through tutorial

--- login.js
+++ login.js
@@ -5,11 +5,11 @@ var user = new Parse.Query("log");
 async function retreiveU(){
     if(uName != ""){  
         try {
-            const uname = await user.get('Uname');
+            const uname = await query.get('Uname');
 	    alert(uname);
             const Pas = await user.get('Pass');
 	    alert(Pas);
-            if(pass !==Pas) {
+            if(pass !== Pas) {
                 wrong();
             }
         }catch(error){

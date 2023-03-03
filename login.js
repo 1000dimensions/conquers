c6bcd9f1535dc36085a3c6b3a701ee8b3f1dff87
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-03 12:45:02 -0500

    one more time

--- login.js
+++ login.js
@@ -5,9 +5,9 @@ var user = new Parse.Query("log");
 async function retreiveU(){
     if(uName != ""){  
         try {
-            const uname = await query.get('Uname');
+            const uname = await user.get('Uname');
 	    alert(uname);
-            const Pas = await user.get('Pass');
+            const Pas = await uname.get('Pass');
 	    alert(Pas);
             if(pass !== Pas) {
                 wrong();

37eb7a6cd8ce3cfbec1f3d3b4fb194266da79dce
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-08 12:35:35 -0500

    Add files via upload

--- login.js
+++ login.js
@@ -2,17 +2,11 @@ const bogga = document.getElementById('ogga');
 const pass = document.getElementById('pass').value;
 const uName = document.getElementById('name').value;
 async function logn(){
-    if(uName != ""){  
-        if(pass != ""){
-	    Parse.User.logIn(this.uName, this.pass).then((resp) => {
-		alert("login Successful", resp);
-	
-	    });
-	    catch(err){
-        	alert(err + "You got an error!");
-            }
-	}
-    }
+   Parse.User.logIn(this.uName, this.pass).then((resp) => {
+      alert("login Successful", resp);
+   })catch(error){
+      alert(error + "You got an error!");
+   }
 }
 bogga.onclick = logn; 
 

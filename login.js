e2df8a5ed3b0eac00b5f5fedc3ef5472b480fcde
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-08 12:28:07 -0500

    Error

--- login.js
+++ login.js
@@ -3,13 +3,15 @@ const pass = document.getElementById('pass').value;
 const uName = document.getElementById('name').value;
 async function logn(){
     if(uName != ""){  
-       if(pass != ""){
-	Parse.User.logIn(this.uName, this.pass).then((resp) => {
-	alert("login Successful", resp);
+        if(pass != ""){
+	    Parse.User.logIn(this.uName, this.pass).then((resp) => {
+		alert("login Successful", resp);
 	
-        }catch(err){
-           alert(err + "You got an error!");
-        }
+	    });
+	    catch(err){
+        	alert(err + "You got an error!");
+            }
+	}
     }
 }
 bogga.onclick = logn; 

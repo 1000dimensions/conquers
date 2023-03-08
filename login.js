9884e48e2ee68ab08368634d4c7fa550a0b86fba
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-08 12:18:48 -0500

    Updated somethings

--- login.js
+++ login.js
@@ -1,20 +1,16 @@
 const bogga = document.getElementById('ogga');
 const pass = document.getElementById('pass').value;
 const uName = document.getElementById('name').value;
-var user = new Parse.Query("log");
 async function logn(){
     if(uName != ""){  
-        try {
-            login = Parse.User
-		    .logIn(uName, pass).then(function(login){
-		    console.log("Logged in!");
-		    
-            });
-        }catch(error){
-           alert(error + "You got an error!");
+       if(pass != ""){
+	Parse.User.logIn(this.uName, this.pass).then((resp) => {
+	alert("login Successful", resp);
+	
+        }catch(err){
+           alert(err + "You got an error!");
         }
     }
 }
-function wrong() {
-    alert("Wrong Password!");
-}
+bogga.onclick = logn; 
+

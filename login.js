93b1de345b662a07a6c8cd08baa458e89f50aaf7
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-09 11:58:27 -0500

    Update login.js

--- login.js
+++ login.js
@@ -3,8 +3,8 @@ const pass = document.getElementById('pass').value;
 const uName = document.getElementById('name').value;
 async function logn(){
    try{
-   Parse.User.logIn(this.uName, this.pass).then((resp) => {
-      alert("login Successful", resp);
+      Parse.User.logIn(this.uName, this.pass).then((resp) => {
+         alert("login Successful", resp);
    });
    }catch(err){
       alert(err + "You got an error!");

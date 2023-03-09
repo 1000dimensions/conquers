d55c022d0f29f296e063e05feed95f7e8ff265c1
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-09 12:01:34 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,8 @@
 const bogga = document.getElementById('ogga');
 const pass = document.getElementById('pass').value;
 const uName = document.getElementById('name').value;
+Parse.initialize("OztB4qjJCEACbkbjGRvHLpT1huB8nncdcRvBIxQ3","TXk2LiJY4FaxVSXp1xkIpSQtxem6TfuJBjQhuwkw");//hush hush about it
+Parse.serverURL = 'https://parseapi.back4app.com/';
 async function logn(){
    try{
       Parse.User.logIn(this.uName, this.pass).then((resp) => {

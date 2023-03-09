3108e9b43a9c9c8f732d1984b7fd30245409676d
Lawson The Neck of the Demigod department <75544834+1000dimensions@users.noreply.github.com>
2023-03-09 12:31:16 -0500

    Update conquers.js

--- conquers.js
+++ conquers.js
@@ -1,10 +1,11 @@
-const cbg = document.querySelector('#map')
-const ctx = cbg.getContext('2d')
-
+const cbg = document.querySelector('#map');
+const ctx = cbg.getContext('2d');
+ctx.fillstyle("green");
+ctx.fillRect(0, 0, cbg.height, cbg.width);
 var time = Date.now();
-var days = time / 1000 / 60 / 60 / 24
+var days = time / 1000 / 60 / 60 / 24;
 var min = time / 1000 / 60;
-var hours = time / 1000 / 60 / 60 / 24
-console.log(min);
-console.log(days);
-console.log(hours);
+var hours = time / 1000 / 60 / 60 / 24;
+alert(min);
+alert(days);
+alert(hours);
